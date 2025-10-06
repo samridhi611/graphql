8ded094391fa62b1790f5469225b866ae06b01f9
Samridhi <wsspl-samridhi@ASCs-MacBook-Air.local>
2025-10-06 07:58:56 +0530

    dirst

---
+++ src/index.js
@@ -0,0 +1,40 @@
+import { ApolloServer } from "@apollo/server";
+import {startStandaloneServer} from "@apollo/server/standalone"
+
+// db
+import db from './_db.js'
+
+// types
+import {typeDefs} from "./schema.js"
+
+const resolvers = {
+    Query: {
+        games() {
+            return db.games
+        },
+        reviews() {
+            return db.reviews
+        },
+        authors() {
+            return db.authors
+        },
+        review(_,args) {
+            args
+            return db.reviews.find((review) => review.id === args.id)
+        }
+    }
+}
+
+// server setup
+const server = new ApolloServer({
+    typeDefs,
+    resolvers
+})
+
+const {url} = await startStandaloneServer(server,{
+    listen : {
+        port : 4000
+    }
+})
+
+console.log('Server ready at port',4000)
---
+++ src/schema.js
@@ -0,0 +1,30 @@
+export const typeDefs = `#graphql
+    type Game {
+        id: ID!
+        title: String!
+        platform: [String!]!
+    }
+    type Review {
+        id: ID!,
+        rating: Int!
+        content: String!
+    }
+    type Author {
+        id: ID!
+        name: String!
+        verified: Boolean!
+    }
+    type Query {
+        reviews: [Review]
+        review(id: ID!): Review
+        games: [Game]
+        game(id: ID!): Game
+        authors: [Author]
+        author(id: ID!): Author
+    }
+`
+
+// int, float, string, boolean, ID [key for data objects]
+// more new types can also be build
+
+// ! -> to mark as required
